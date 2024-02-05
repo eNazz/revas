d6a2fe40e6bc9a8b093c169f075a8e53906672db
Esteban <esteban.nazer87@gmail.com>
2024-02-05 09:51:16 -0300

    Update cambios estilos definidos, servicios como tarjetas en main, servicios y novedades a desarrollar, imagenes en cada seccion.

--- js/main.js
+++ js/main.js
@@ -59,6 +59,16 @@ AOS.init({
 	};
 	loader();
 
+	// loader
+/* 	var loader = function () {
+		setTimeout(function () {
+			if ($('#loader').length > 0) {
+				$('#loader').removeClass('show');
+			}
+		}, 1);
+	};
+	loader(); */
+
 	// Scrollax
 	$.Scrollax();
 
