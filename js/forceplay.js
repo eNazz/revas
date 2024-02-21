f4c772bb1e3f9daf7a07d9211785f8f12049c40b
Esteban <esteban.nazer87@gmail.com>
2024-02-21 12:51:04 -0300

    test video iphone

---
+++ js/forceplay.js
@@ -0,0 +1,13 @@
+function makeURL(object) {
+    return (window.URL) ? window.URL.createObjectURL(object) :    
+    window.webkitURL.createObjectURL(object);
+  }
+
+  async function display(videoStream){
+    var myvideo = document.getElementById('ForcePlay');
+    let blob = await fetch(videoStream).then(r => r.blob());
+    var videoUrl= makeURL(blob);
+    myvideo.src = videoUrl;
+  }
+
+  display('./assets/video_home.mp4');
