87bcf75b7dbfaa3ec1a47607df6bc6ec7fab3191
Esteban <esteban.nazer87@gmail.com>
2024-02-28 13:26:55 -0300

    feat: nueva seccion preguntas frecuentes

--- js/accordion.js
+++ js/accordion.js
@@ -1,9 +1,8 @@
-let myAccordion = new myAccordion({
-    elements: '.card-freq article',          
+document.addEventListener("DOMContentLoaded", function () {
+  var myAccordion = new gianniAccordion({
+    elements: '.expandableItem',
     trigger: "[data-accordion-element-trigger]",
     content: "[data-accordion-element-content]",
-    // collapsedHeight: '135px' -> maybe you have only one accordion and the content is partial visible
-    // selectedClass: 'my-custom-selected-class' -> by default is 'selected'
-    // openAtLandingIndex: 2 -> set 3rd element open at landing
+    // Otiones adicionales si las necesitas
   });
-  
+});
